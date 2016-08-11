b271ef9840686784c472bfda477c5bcd2c6a5e86
Mike Pi <mikeh@electroteach.com>
2016-08-12 09:48:28 +1000

    some more explanatory documentation

--- LED.java
+++ LED.java
@@ -15,16 +15,16 @@ public class LED
 {
     // The LED is implemented by a Pi4J output pin.
     private GpioPinDigitalOutput ledPin;
-    
+
     //An array which maps the GPIO pin numbers to integers
     private static final Pin[] pinMap = new Pin[] {RaspiPin.GPIO_00, RaspiPin.GPIO_01, RaspiPin.GPIO_02, RaspiPin.GPIO_03, RaspiPin.GPIO_04, 
-        RaspiPin.GPIO_05, RaspiPin.GPIO_06, RaspiPin.GPIO_07, RaspiPin.GPIO_08, RaspiPin.GPIO_09, RaspiPin.GPIO_10, 
-        RaspiPin.GPIO_11, RaspiPin.GPIO_12, RaspiPin.GPIO_13, RaspiPin.GPIO_14, RaspiPin.GPIO_15, RaspiPin.GPIO_16, 
-        RaspiPin.GPIO_17, RaspiPin.GPIO_18, RaspiPin.GPIO_19, RaspiPin.GPIO_20};
+            RaspiPin.GPIO_05, RaspiPin.GPIO_06, RaspiPin.GPIO_07, RaspiPin.GPIO_08, RaspiPin.GPIO_09, RaspiPin.GPIO_10, 
+            RaspiPin.GPIO_11, RaspiPin.GPIO_12, RaspiPin.GPIO_13, RaspiPin.GPIO_14, RaspiPin.GPIO_15, RaspiPin.GPIO_16, 
+            RaspiPin.GPIO_17, RaspiPin.GPIO_18, RaspiPin.GPIO_19, RaspiPin.GPIO_20};
 
     //The default pin, used by the no-parameters constructor.
     private final static int defaultPin = 6;
-   
+
     /**
      * Connect to an LED attached to the default GPIO pin (6).
      * 
@@ -33,21 +33,25 @@ public class LED
     {
         this(defaultPin);
     }
-    
+
     /**
      * Connect to an LED attached to the given pin number.
      * 
      */
     public LED(int pin)
     {
+        // observed 12/8/16  that log fileshowing creation of instances log3.txt shows that the
+        // name of the instance can have nothing to do with the number of
+        // the GPIO pin that the instance is connected to. 
         // Pin allocation is handled by a GpioController. So first we have to get that from the Factory.
+        // LEDButton.LED lED6 = new LEDButton.LED(0);
         GpioController gpio = GpioFactory.getInstance();
-        
+
         // Now we need to ask the Controller to allocate (provision) the pin for us as a Digital Output pin. 
         // The parameter PinState.LOW means that initially, we want to set this pin to LOW (off).
         ledPin = gpio.provisionDigitalOutputPin(pinMap[pin], PinState.LOW);
     }
-    
+
     /**
      * Turn on the LED.
      * 
@@ -56,7 +60,7 @@ public class LED
         // Sets the LED pin state to 1 (high)
         ledPin.high();
     }
-    
+
     /**
      * Turn off the LED.
      * 
@@ -65,7 +69,7 @@ public class LED
         // Sets the LED pin state to (low)
         ledPin.low();
     }
-    
+
     /**
      * Checks if the the LED is on.
      * 
